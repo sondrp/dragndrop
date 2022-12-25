5e9ad25985975afe3ed7cfc96427730b357af482
sondrp <sondrp@stud.ntnu.no>
2022-12-25 16:14:07 +0000

    Setup drag and drop project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DragDrop from './components/DragDrop';
+
+
+function App() {
+  return (
+    <div className='app'>
+      <DragDrop />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DragDrop.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["html"];
+
+function DragDrop() {
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+    console.log(file)
+  };
+
+
+  return (
+    <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+  );
+}
+
+export default DragDrop;
