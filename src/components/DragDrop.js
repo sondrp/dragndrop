720a818a79f21fd533cce3f900eb432fa30693dd
sondrp <sondrp@stud.ntnu.no>
2022-12-25 16:28:34 +0000

    make file reader

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -7,7 +7,14 @@ function DragDrop() {
   const [file, setFile] = useState(null);
   const handleChange = (file) => {
     setFile(file);
-    console.log(file)
+    if (file) {
+        var reader = new FileReader();
+        reader.readAsText(file, "UTF-8")
+        reader.onload = function (evt) {
+            document.getElementById("fileContents").innerHTML = evt.target.result;
+        }
+        console.log(reader.readyState)
+    }
   };
 
 
