3600f93895c74dd6062f0a22ccc12d5751c71562
sondrp <sondrp@stud.ntnu.no>
2022-12-26 09:52:38 +0000

    working splitter

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -1,36 +1,49 @@
 import React, { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 
-const fileTypes = ["html"];
 
-function DragDrop() {
+function getHeaders(line) {
+  return line.split("</th><th>").map(i => i.replace(/<[^>]+>/g, ""))
+}
+
+function getItems(line) {
+  let linearray = line.split("</tr><tr>").map(i => i.split("</td><td>").map(e => e.replace(/<[^>]+>/g, "")))
+  console.log(linearray)
+}
+
+
+
+export default function DragDrop() {
   const [file, setFile] = useState(null);
   const handleChange = (file) => {
     setFile(file);
-
 
 
     if (file) {
         var reader = new FileReader();
         reader.readAsText(file, "UTF-8")
         reader.onload = function (evt) {
-/*             document.getElementById("fileContents").innerHTML = evt.target.result; */
-          var stripped = reader.result.replace(/<[^>]+>/g, "");
 
+          let stripped = reader.result
+          //var stripped = reader.result.replace(/<[^>]+>/g, "");
 
+          // line.substring(0, 5) === "Retur")
           for (let line of stripped.split("\n")) {
-            if (line.substring(0, 5) === "Retur") {
-              console.log(line)
+            if (line.includes("Returnummer")) {
+              let rows = line.split("</th></tr>")
+
+              const headers = getHeaders(rows[0])
+              const items = getItems(rows[1])
+
+
+
             }
           }
         }
     }
   };
-
-
+  
   return (
-    <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+    <FileUploader handleChange={handleChange} name="file" types={["html"]} />
   );
 }
-
-export default DragDrop;
